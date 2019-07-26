9d6d569a1b27f21b42e3419280f03b0a003e7cf9
Vrushab Shandilya <vrushab.shandilya@philips.com>
2019-07-26 10:53:18 +0530

    test pmd

--- helloworld/src/main/java/com/philips/bootcamp/helloworld/App.java
+++ helloworld/src/main/java/com/philips/bootcamp/helloworld/App.java
@@ -1,5 +1,7 @@
 package com.philips.bootcamp.helloworld;
 
+import java.io.*;
+
 /**
  * Hello world!
  *
@@ -12,7 +14,35 @@ public class App
     }
     
     public static void main(String args[]) {
-    	App obj = new App();
-    	obj.printMessage();
+//    	App obj = new App();
+//    	obj.printMessage();
+    	
+    	String s = null;
+    	
+    	try {
+    		Process p = Runtime.getRuntime().exec("cmd /c pmd -d App.java -f text -R ../../../../rulesets/basic.xml");
+            
+            BufferedReader stdInput = new BufferedReader(new 
+                 InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new 
+                 InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }    		
+    	} catch(IOException e) {
+    		System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+    	}    	        	
     }
 }
