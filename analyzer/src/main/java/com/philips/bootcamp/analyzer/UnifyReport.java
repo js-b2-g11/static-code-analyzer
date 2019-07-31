b21a967f0bca700f4f4f7edd1722b46a5c202185
Vrushab Shandilya <vrushab.shandilya@philips.com>
2019-07-31 15:47:48 +0530

    add timestamp feature

--- analyzer/src/main/java/com/philips/bootcamp/analyzer/UnifyReport.java
+++ analyzer/src/main/java/com/philips/bootcamp/analyzer/UnifyReport.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.Date;
 
 import com.philips.bootcamp.utils.Values;
 
@@ -13,7 +15,7 @@ public class UnifyReport {
     public static int countPmdReportlines=0;
     public static int countCheckstyleReportlines=0;
 	
-	public static void mergeReports(PmdReportGenerator pmdReport, CheckstyleReportGenerator checkstyleReport) {
+	public static void mergeReports(PmdReportGenerator pmdReport, CheckstyleReportGenerator checkstyleReport, Timestamp[] reportTimeStamps) {
 		try {
 			PrintWriter pw = new PrintWriter(Values.FINAL_OUTPUT_FILE);                 
 			
@@ -23,12 +25,14 @@ public class UnifyReport {
 			  
 			String line1 = br1.readLine(); 
 			String line2 = br2.readLine();                       
+						
 			
 			// 2 loops to copy lines of both reports into a single file 			
 			
-			pw.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
-			pw.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tPMD Report");
-			pw.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
+			pw.println("-----------------------------------------------------------------------------------------------");
+			pw.println("\t\t\t\t\t\t\t\t\tPMD Report");
+			pw.println("-----------------------------------------------------------------------------------------------\n\n");			
+			pw.println("PMD report start time: " + reportTimeStamps[0] + "\n");
 			while (line1 != null) 
 			{             	
 			    if(line1 != null) 
@@ -37,10 +41,12 @@ public class UnifyReport {
 			        line1 = br1.readLine();
 			        countPmdReportlines++;
 			    }
-			}
-			pw.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------");
-			pw.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tCheckstyle Report");
-			pw.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			}			
+			pw.println("PMD report end time: " + reportTimeStamps[1] + "\n");
+			pw.println("-----------------------------------------------------------------------------------------------");
+			pw.println("\t\t\t\t\t\t\t\t\tCheckstyle Report");
+			pw.println("-----------------------------------------------------------------------------------------------\n\n");
+			pw.println("Checkstyle report start time: " + reportTimeStamps[2] + "\n");
 			while (line2 != null)
 			{
 			    if(line2 != null) 
@@ -50,7 +56,7 @@ public class UnifyReport {
 			        countCheckstyleReportlines++;
 			    } 
 			} 
-     
+			pw.println("Checkstyle report end time: " + reportTimeStamps[3] + "\n");
 			pw.flush(); 
 			  
 			// closing resources 
--- analyzer/src/main/java/com/philips/bootcamp/analyzer/ValidateAndMergeFile.java
+++ analyzer/src/main/java/com/philips/bootcamp/analyzer/ValidateAndMergeFile.java
@@ -1,15 +1,36 @@
 package com.philips.bootcamp.analyzer;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class ValidateAndMergeFile{
     public void checkFile(String filepath) {
         if (InputFile.isValidPath(filepath)) {
 
-            System.out.print("Valid file path detected!\n");
-            PmdReportGenerator reportPmd = new PmdReportGenerator(filepath);
-            reportPmd.generateReport();
+            System.out.println("Valid file path detected!");
+                           		
+			Timestamp pmdStartTime, pmdEndTime, checkstyleStartTime, checkstyleEndTime;
+			
+            PmdReportGenerator reportPmd = new PmdReportGenerator(filepath); 
+            
+			pmdStartTime = new Timestamp(new Date().getTime());			
+            System.out.println(pmdStartTime);
+			reportPmd.generateReport();
+            
+			pmdEndTime = new Timestamp(new Date().getTime());                      
+			System.out.println(pmdEndTime);
+			
             CheckstyleReportGenerator reportCheckstyle = new CheckstyleReportGenerator(filepath);
+                        
+            checkstyleStartTime = new Timestamp(new Date().getTime());
+            System.out.println(checkstyleStartTime);
             reportCheckstyle.generateReport();
-            UnifyReport.mergeReports(reportPmd, reportCheckstyle);
+            
+            checkstyleEndTime = new Timestamp(new Date().getTime());
+            System.out.println(checkstyleEndTime);
+            Timestamp[] reportTimeStamps = {pmdStartTime, pmdEndTime, checkstyleStartTime, checkstyleEndTime};
+            
+            UnifyReport.mergeReports(reportPmd, reportCheckstyle, reportTimeStamps);
 
         } else {
 
