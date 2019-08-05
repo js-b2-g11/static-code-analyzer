f2265e648bc3afbe38a043c6c576b8122dd79259
Vrushab Shandilya <vrushab.shandilya@philips.com>
2019-08-05 23:31:31 +0530

    remove comments, change method name: parser, delete unifyreport

--- analyzer/src/main/java/com/philips/bootcamp/analyzer/PmdReportGenerator.java
+++ analyzer/src/main/java/com/philips/bootcamp/analyzer/PmdReportGenerator.java
@@ -9,7 +9,6 @@ import java.sql.Timestamp;
 import java.util.Properties;
 
 import com.philips.bootcamp.utils.FileValidator;
-import com.philips.bootcamp.utils.Values;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -71,47 +70,4 @@ public class PmdReportGenerator extends Tool{
 		return (FileValidator.isValidPath(this.getFilepath()));
 	}
 	
-//	public static void main(String args[]) throws FileNotFoundException, IOException {
-//		String propertiesFile = "./../sca.properties";		
-//		PmdReportGenerator reportPmd = PmdReportGenerator.getPmdReportObject(propertiesFile);
-//		PmdReportGenerator reportPmd = new PmdReportGenerator("akdshflkjadslf");
-//		reportPmd.generateReport();
-//	}
-	
-//	private String pmd;
-//	private String filepath;
-//    private String format;
-//    private String ruleset;
-//    private String outputFile;
-//    private Timestamp reportStartTime;
-//    private Timestamp reportEndTime;
-//    
-//    public PmdReportGenerator(String filepath) {
-//    	this.filepath = filepath;
-//    	this.pmd = Values.PMD_CMD;
-//    	this.format = "text";
-//    	this.ruleset = Values.PMD_RULESET;
-//    	this.outputFile = Values.PMD_OUTPUT_FILE;
-//    }
-//        
-//	public void generateReport() throws IOException, InterruptedException{		
-//		
-//		if(filepath == null || filepath.trim().length() == 0) {
-//			try {
-//	            throw new Exception("Filepath is not specified enter a valid filepath");
-//	        } catch (Exception e) {
-////	            e.printStackTrace();
-//	            System.out.print("Filepath not specified\n");
-//	        } 
-//		}
-//		else 
-//        {
-//            String executePmdString = pmd + filepath + " -f "+ format +" -R " + ruleset;
-//            Process pmdProcess = Runtime.getRuntime().exec(executePmdString + " -r " + outputFile);
-//            
-//            pmdProcess.waitFor();
-//            
-//            System.out.print("PMD report generated\n");
-//        }
-//	}
 }
